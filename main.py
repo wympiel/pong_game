dee0056fc4d64a09ccb1aff6699f881cace2bb05
wympiel <wymp10@gmail.com>
2021-01-02 09:21:03 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(width=800, height=600)
+screen.title('Pong')
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detecting collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    # Collision with l_paddle
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect r_paddle misses
+
+    if ball.xcor() > 380 or ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect when l_paddle misses
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    
+screen.exitonclick()
